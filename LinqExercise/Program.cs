6edffd2ab48ac3567d83cafcfede86c98f405458
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-29 14:12:42 -0500

    finished TODOs

--- LinqExercise/Program.cs
+++ LinqExercise/Program.cs
@@ -24,31 +24,94 @@ namespace LinqExercise
              */
 
             //Print the Sum and Average of numbers
+            Console.WriteLine("Sum:");
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine("\nAverage:");
+            Console.WriteLine(numbers.Average());
 
             //Order numbers in ascending order and decsending order. Print each to console.
+            var ascArray = numbers.OrderBy(num => num);
+            var descArray = numbers.OrderByDescending(num => num);
+
+            Console.WriteLine("\nAscending array:");
+            foreach (var num in ascArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("\nDescending array:");
+            foreach (var num in descArray)
+            {
+                Console.WriteLine(num);
+            }
 
             //Print to the console only the numbers greater than 6
+            var moreThanSix = numbers.Where(num => num > 6);
+
+            Console.WriteLine("\nNumbers greater than 6:");
+            foreach (var num in moreThanSix)
+            {
+                Console.WriteLine(num);
+            }
 
             //Order numbers in any order (acsending or desc) but only print 4 of them **foreach loop only!**
+            var onlyFour = ascArray.Take(4);
+
+            Console.WriteLine("Take 4 from ascending array:");
+            foreach (var num in onlyFour)
+            {
+                Console.WriteLine(num);
+            }
 
             //Change the value at index 4 to your age, then print the numbers in decsending order
+            numbers[4] = 25;
+            descArray = numbers.OrderByDescending(num => num);
+
+            Console.WriteLine("New descending array with my age added:");
+            foreach (var num in descArray)
+            {
+                Console.WriteLine(num);
+            }
 
             // List of employees ***Do not remove this***
             var employees = CreateEmployees();
 
             //Print all the employees' FullName properties to the console only if their FirstName starts with a C OR an S.
             //Order this in acesnding order by FirstName.
+            var csNames = employees.Where(name => name.FirstName[0] == 'C' || name.FirstName[0] == 'S').OrderBy(name => name.FirstName);
+
+            Console.WriteLine("\nC or S employee names: ");
+            foreach (var name in csNames)
+            {
+                Console.WriteLine(name.FullName);
+            }
 
             //Print all the employees' FullName and Age who are over the age 26 to the console.
             //Order this by Age first and then by FirstName in the same result.
 
+            var olderThan26 = employees.Where(name => name.Age > 26).OrderBy(name => name.Age).ThenBy(name => name.FirstName);
+
+            Console.WriteLine("\nEmployees over the age of 26:");
+            foreach(var name in olderThan26)
+            {
+                Console.WriteLine(name.FullName + " " + name.Age);
+            }
+
             //Print the Sum and then the Average of the employees' YearsOfExperience
             //if their YOE is less than or equal to 10 AND Age is greater than 35
+            var yoeEmployees = employees.Where(name => name.YearsOfExperience <= 10 && name.Age > 35);
+
+            Console.WriteLine("\nYOE Sum:");
+            Console.WriteLine(yoeEmployees.Sum(name => name.YearsOfExperience));
+
+            Console.WriteLine("\nYOE Average:");
+            Console.WriteLine(yoeEmployees.Average(name => name.YearsOfExperience));
 
             //Add an employee to the end of the list without using employees.Add()
+            var newEmployeeList = employees.Append(new Employee("Greatest", "Ever", 9001, 1)).ToList();
 
-            
-            Console.WriteLine();
+            Console.WriteLine("\nMy new employee:");
+            Console.WriteLine(newEmployeeList[newEmployeeList.Count - 1].FullName);
 
             Console.ReadLine();
         }
